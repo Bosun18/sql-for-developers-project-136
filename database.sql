16a21bc135e0614d2e1bfb46488f62924ac9183f
bosun36 <sviridenko@3davinci.ru>
2024-11-03 15:33:43 +0300

    Fixed

--- database.sql
+++ database.sql
@@ -1,34 +1,3 @@
--- Таблица программ
-CREATE TABLE programs (
-                          id SERIAL PRIMARY KEY,
-                          title VARCHAR(100) NOT NULL,
-                          price DECIMAL(10, 2) NOT NULL,
-                          program_type VARCHAR(50) NOT NULL, -- интенсив, профессия и т.д.
-                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          is_deleted BOOLEAN DEFAULT FALSE
-);
-
--- Таблица модулей
-CREATE TABLE modules (
-                         id SERIAL PRIMARY KEY,
-                         title VARCHAR(100) NOT NULL,
-                         description TEXT NOT NULL,
-                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                         is_deleted BOOLEAN DEFAULT FALSE
-);
-
--- Таблица курсов
-CREATE TABLE courses (
-                         id SERIAL PRIMARY KEY,
-                         title VARCHAR(100) NOT NULL,
-                         description TEXT NOT NULL,
-                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                         is_deleted BOOLEAN DEFAULT FALSE
-);
-
 -- Таблица уроков
 CREATE TABLE lessons (
                          id SERIAL PRIMARY KEY,
@@ -41,6 +10,46 @@ CREATE TABLE lessons (
                          course_id INT NOT NULL,
                          is_deleted BOOLEAN DEFAULT FALSE,
                          FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
+);
+
+-- Таблица курсов
+CREATE TABLE courses (
+                         id SERIAL PRIMARY KEY,
+                         title VARCHAR(100) NOT NULL,
+                         description TEXT NOT NULL,
+                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         is_deleted BOOLEAN DEFAULT FALSE
+);
+
+-- Таблица модулей
+CREATE TABLE modules (
+                         id SERIAL PRIMARY KEY,
+                         title VARCHAR(100) NOT NULL,
+                         description TEXT NOT NULL,
+                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         is_deleted BOOLEAN DEFAULT FALSE
+);
+
+-- Таблица программ
+CREATE TABLE programs (
+                          id SERIAL PRIMARY KEY,
+                          title VARCHAR(100) NOT NULL,
+                          price DECIMAL(10, 2) NOT NULL,
+                          program_type VARCHAR(50) NOT NULL, -- интенсив, профессия и т.д.
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          is_deleted BOOLEAN DEFAULT FALSE
+);
+
+-- Связующая таблица для модулей и уроков (многие-ко-многим)
+CREATE TABLE lesson_module (
+                               lesson_id INT NOT NULL,
+                               module_id INT NOT NULL,
+                               PRIMARY KEY (lesson_id, module_id),
+                               FOREIGN KEY (lesson_id) REFERENCES lessons(id) ON DELETE CASCADE,
+                               FOREIGN KEY (module_id) REFERENCES modules(id) ON DELETE CASCADE
 );
 
 -- Связующая таблица для курсов и модулей (многие-ко-многим)
@@ -61,23 +70,6 @@ CREATE TABLE program_module (
                                 FOREIGN KEY (module_id) REFERENCES modules(id) ON DELETE CASCADE
 );
 
--- Связующая таблица для модулей и уроков (многие-ко-многим)
-CREATE TABLE lesson_module (
-                               lesson_id INT NOT NULL,
-                               module_id INT NOT NULL,
-                               PRIMARY KEY (lesson_id, module_id),
-                               FOREIGN KEY (lesson_id) REFERENCES lessons(id) ON DELETE CASCADE,
-                               FOREIGN KEY (module_id) REFERENCES modules(id) ON DELETE CASCADE
-);
-
--- Таблица учебных групп
-CREATE TABLE teaching_groups (
-                                 id SERIAL PRIMARY KEY,
-                                 slug VARCHAR(100) NOT NULL UNIQUE,
-                                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 -- Таблица пользователей
 CREATE TABLE users (
                        id SERIAL PRIMARY KEY,
@@ -89,6 +81,14 @@ CREATE TABLE users (
                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                        FOREIGN KEY (teaching_group_id) REFERENCES teaching_groups(id) ON DELETE CASCADE
+);
+
+-- Таблица учебных групп
+CREATE TABLE teaching_groups (
+                                 id SERIAL PRIMARY KEY,
+                                 slug VARCHAR(100) NOT NULL UNIQUE,
+                                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Таблица подписок
